4afddc51b73a27e9d8d756cebdd52965f4bc75bd
naufalzaid <naufalzaid17@gmail.com>
2020-09-04 23:17:40 +0700

    First Commit

---
+++ tipedata.py
@@ -0,0 +1,26 @@
+print("Wilujeng Sumping")
+print("Welcome", "Mobile", sep='_')
+print("Welcome", "Mobile", "Legend", sep='_', end='\t')
+print("Indonesia")
+print("meskipun \
+kita berada di indonesia")
+print("anda mau beli ayam \\ nasi")
+print("it's a gun")
+print('it\'s a gun')
+print("katakan \"PETA!!\"")
+
+print(34564E3)
+
+# integer div 
+print(10 // 4)
+print(-10 // 4)
+print(10 // 4.)
+
+print(end='\n')
+
+# modulo
+print(10 % 4)
+print(10 % 4.5)
+# print(12 % -5)
+# modulus masi bingung
+
